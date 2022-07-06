374f3a7544e1ad736a18e16fed60fb129feb5efb
Home Asus <doyijian@163.com>
2022-07-06 21:29:25 +0800

    add more icon

--- main.js
+++ main.js
@@ -10,32 +10,37 @@ const App = {
                             name: '中央气象台',
                             site: 'http://www.nmc.cn/',
                             desc: '中央气象台的官方网站',
-                            icon: 'img/f.png'
+                            icon: 'img/i1.png'
                         },
                         {
                             name: '降水实况',
                             site: 'http://www.nmc.cn/publish/observations/hourly-precipitation.html',
-                            desc: '降水实况'
+                            desc: '降水实况',
+                            icon: 'img/water.png'
                         },
                         {
                             name: '气温预报',
                             site: 'http://www.nmc.cn/publish/temperature/hight/24hour.html',
-                            desc: '气温预报'
+                            desc: '气温预报',
+                            icon: 'img/temp.png'
                         },
                         {
                             name: '雷达图',
                             site: 'http://www.nmc.cn/publish/radar/chinaall.html',
-                            desc: '雷达图'
+                            desc: '可调类型的雷达图',
+                            icon: 'img/radar.png'
                         },
                         {
                             name: '空气质量',
                             site: 'http://www.nmc.cn/publish/environment/air_pollution-24.html',
-                            desc: '空气质量'
+                            desc: '空气质量',
+                            icon: 'img/air.png'
                         },
                         {
                             name: '台风',
                             site: 'http://www.nmc.cn/publish/typhoon/probability-img2.html',
-                            desc: '台风'
+                            desc: '预告台风路径图',
+                            icon: 'img/ty.png'
                         },
                     ]
                 },
@@ -45,10 +50,57 @@ const App = {
                         {
                             name: '可视化地图',
                             site: 'http://www.weather.com.cn/radar/',
-                            desc: '中国天气网的多功能可视化地图'
+                            desc: '多功能可视化地图',
+                            icon: 'img/map.png'
+                        },
+                        {
+                            name: '雷达拼图',
+                            site: 'http://www.weather.com.cn/radar/index2021.shtml',
+                            desc: '常见的各类卫星拼图图片',
+                            icon: 'img/radar.png'
+                        },
+                        {
+                            name: '中长期天气预报',
+                            site: 'http://products.weather.com.cn/yb.html?page=YB&type=YB_TQQS_10D',
+                            desc: '更长时期持续预报',
+                            icon: 'img/long.png'
+                        },
+                        {
+                            name: '全国降水预报',
+                            site: 'http://products.weather.com.cn/sk.html?page=YB&type=YB_JSL_024',
+                            desc: '降水预报片',
+                            icon: 'img/rain.png'
+                        },
+                    ]
+                },
+                {
+                    groupname: '灾害天气',
+                    groupsite: [
+                        {
+                            name: '实时台风卫星图',
+                            site: 'http://typhoon.nmc.cn/web.html',
+                            desc: '追踪台风的实景地图',
+                            icon: 'img/satelite.png'
+                        },
+                        {
+                            name: '实时台风地图',
+                            site: 'http://typhoon.weather.com.cn/gis/typhoon_full.shtml',
+                            desc: '追踪台风的政区图',
+                            icon: 'img/map2.png'
+                        },
+                    ]
+                },
+                {
+                    groupname: '其它',
+                    groupsite: [
+                        {
+                            name: 'IBM 天气',
+                            site: 'https://weather.com/zh-CN/weather/today/',
+                            desc: 'IBM 天气数据源',
+                            icon: 'img/ibm.png'
                         }
                     ]
-                }
+                },
             ]
         }
     },
